4b1ff3bb1cd3950f875a22dcb5b7f5d207d33f33
BalArtemiy <blackpawsstudio@gmail.com>
2020-12-21 23:14:35 +0300

    start

---
+++ wiki/encyclopedia/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from . import util
+
+from markdown2 import markdown
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def title(request, file):
+    with open(f"entries/{file}.md") as f:
+         md_text = f.read()
+         with open("encyclopedia/templates/encyclopedia/temp.html", 'w') as fw:
+            st=(markdown(md_text))
+            with open("encyclopedia/static/encyclopedia/b.html", 'r') as b:
+                b_text=b.read()
+            st=b_text+st
+            fw.write(st)
+    return render(request, "encyclopedia/temp.html") 
