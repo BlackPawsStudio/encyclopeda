10750b697e082d7de7fefce84d520a010c838346
BalArtemiy <blackpawsstudio@gmail.com>
2020-12-23 19:58:55 +0300

    random now changes url

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("page/"'<file>', views.title),
-    path("randompage", views.rand, name="rand")
+    # path("page/"'<file>', views.title),
+    path("page/"'<title>'"/", views.search),
+    path("page/"'<title>'"/edit", views.editpage),
+    path("saving_changes",views.save)
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,36 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from . import util
 import random
+from django.shortcuts import redirect
 
 from markdown2 import markdown
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    entries=util.list_entries()
+    path=random.choice(entries)
+    data={"entries":entries, "path":path}
+    return render(request, "encyclopedia/layout.html", context=data)
 
-def title(request, file):
-    with open(f"entries/{file}.md") as f:
-         md_text = f.read()
-         with open("encyclopedia/templates/encyclopedia/temp.html", 'w') as fw:
-            st=(markdown(md_text))
+def search(request, title):
+    text = markdown(util.get_entry(title))
+    path=random.choice(util.list_entries())
+    if not text:
+        return HttpResponse("Nothing found", status=404)
+    else:
+        with open("encyclopedia/templates/encyclopedia/temp.html", 'w') as fw:
             with open("encyclopedia/static/encyclopedia/b.html", 'r') as b:
                 b_text=b.read()
-            st=b_text+'\n'+st
-            fw.write(st)
-            print(st)
-    return render(request, "encyclopedia/temp.html") 
+            text=b_text+'\n'+text
+            fw.write(text)
+    return render(request, "encyclopedia/temp.html", context={"path":path})
 
-def rand(request):
-    entr = util.list_entries()
-    p = random.choice(entr)    
-    with open(f"entries/{p}.md") as f:
-         md_text = f.read()
-         with open("encyclopedia/templates/encyclopedia/temp.html", 'w') as fw:
-            st=(markdown(md_text))
-            with open("encyclopedia/static/encyclopedia/b.html", 'r') as b:
-                b_text=b.read()
-            st=b_text+st
-            fw.write(st)
-    return render(request, "encyclopedia/temp.html") 
+def editpage(request,title):
+    content=util.get_entry(title)
+    data = {'title':title, "content":content}
+    return render(request, "encyclopedia/editpage.html", context=data)
+
+def save(request):
+    
+    return HttpResponse.entry
